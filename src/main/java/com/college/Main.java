5dbe479bc5c7c866e2f2a54eff1827a580824f1a
JJ-Twigg <222153881@mycput.ac.za>
2025-03-12 17:13:22 +0200

    Croissant...2

--- src/main/java/com/college/Main.java
+++ src/main/java/com/college/Main.java
@@ -22,6 +22,6 @@ package com.college;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world2!");
     }
 }
