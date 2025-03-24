223c1a28be13d30ecb59ce20d07d3c4dace5c04b
taliasmuts28 <taliasmuts28@gmail.com>
2025-03-24 18:44:41 +0200

    updates to test and paymentfactory

--- src/main/java/com/college/domain/Main.java
+++ src/main/java/com/college/domain/Main.java
@@ -5,7 +5,7 @@ import com.college.factory.PaymentFactory;
 public class Main {
     public static void main(String[] args) {
         // Create a Payment object using the Builder pattern
-        Payment defaultPayment = PaymentFactory.createDefaultPayment(1, "1500.00", "Credit Card", "Completed", "2023-10-01");
+        Payment defaultPayment = PaymentFactory.createDefaultPayment(1, "1500.00");
         System.out.println("Default Payment: " + defaultPayment);
 
         // Create a custom payment using the factory
